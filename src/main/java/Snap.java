6fbefac5a230c2bdaf62e3ee354622026715f580
Kayley <kayley.whitfield1@gmail.com>
2026-02-09 19:54:21 +0000

    Game is now two-player with the player who draws the matching card being the victor

--- src/main/java/Snap.java
+++ src/main/java/Snap.java
@@ -5,6 +5,7 @@ public class Snap extends CardGame{
     Scanner scanner;
     Card lastCard;
     Card currentCard;
+    boolean player1 = false;
 
     Snap() {
         super("Snap");
@@ -13,18 +14,26 @@ public class Snap extends CardGame{
     void start(){
         scanner = new Scanner(System.in);
         shuffleDeck();
-        System.out.println("Welcome to snap! Press enter to draw a card. \nThe game will end when a matching symbol is drawn.");
+        System.out.println("\nWelcome to snap! Press enter to draw a card. \nThe game will end when a matching symbol is drawn.");
         nextTurn();
     }
 
+    String getPlayer(){
+        if(player1){
+            return "Player 1";
+        }
+        return "Player 2";
+    }
+
     void nextTurn(){
+        player1 = !player1;
         scanner.nextLine();
         lastCard = currentCard;
         currentCard = dealCard();
-        System.out.println("You draw: "+currentCard);
+        System.out.print(getPlayer()+ " draws " +currentCard);
         if(lastCard!=null){
             if(currentCard.suit.equals(lastCard.suit)){
-                System.out.println("Snap! You win!");
+                System.out.println("\nSnap! "+ getPlayer() + " wins!");
             }
             else {
                 nextTurn();
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,5 +3,6 @@ public class main {
     static void main(String[] args) {
         Snap game = new Snap();
         game.start();
+
     }
 }
